c61c80e21fce9dc082ff873423a13bc8fe8d3a6f
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-07-10 19:56:03 +0300

    Installed Isotope files and updated package.json

--- PipeBuild/PipeBuild/wwwroot/js/site.js
+++ PipeBuild/PipeBuild/wwwroot/js/site.js
@@ -180,48 +180,6 @@ $(document).ready(function () {
             $(".navbar-sticky").addClass("top-nav-collapse");
         };
     });
-
-
-    /* --------------------------------------------------------
-    ISOTOPE
-   ----------------------------------------------------------- */
-    $(function () {
-        var $portfolio = $('.masonary');
-        $portfolio.isotope({
-            masonry: {
-            }
-        });
-
-        var $optionSets = $('.option-set'),
-            $optionLinks = $optionSets.find('a');
-
-        $optionLinks.click(function () {
-            var $this = $(this);
-            // don't proceed if already selected
-            if ($this.hasClass('selected')) {
-                return false;
-            }
-            var $optionSet = $this.parents('.option-set');
-            $optionSet.find('.selected').removeClass('selected');
-            $this.addClass('selected');
-            var options = {},
-                key = $optionSet.attr('data-option-key'),
-                value = $this.attr('data-option-value');
-            // parse 'false' as false boolean
-            value = value === 'false' ? false : value;
-            options[key] = value;
-            if (key === 'layoutMode' && typeof changeLayoutMode === 'function') {
-                // changes in layout modes need extra logic
-                changeLayoutMode($this, options)
-            } else {
-                // otherwise, apply new options
-                $portfolio.isotope(options);
-            }
-
-            return false;
-        });
-
-    });
     
     /* --------------------------------------------------------
     COUNT TO
